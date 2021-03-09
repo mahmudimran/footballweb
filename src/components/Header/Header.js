4c269e64812b8c4e64181ef8ca058fe1dfa07178
mahmud imran <mahmudimran203@gmail.com>
2021-03-10 01:22:42 +0600

    start

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1> Team Player</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/League/League.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const League = (props) => {
+    return (
+        <div>
+            <h4> my fvt club </h4>
+        </div>
+    );
+};
+
+export default League;
